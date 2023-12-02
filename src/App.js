8eaf43313bae5a98627dd281d24ab585ee111a49
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-03 01:11:13 +0530

    Setup Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { AiFillAlipayCircle } from "react-icons/ai";
+import { motion } from "framer-motion";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.h1 
+      className="text-3xl font-bold underline text-red-500 bg-blue-500"
+      animate={{
+        opacity: 0,
+        transition: {
+          delay: 1
+        }
+      }}
+    >
+      Hello world! <AiFillAlipayCircle />
+    </motion.h1>
   );
 }
 
